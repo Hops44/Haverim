e1b544cbd035a7c38bada5dc54ed16993274672e
Omer Nahum <omdan784@gmail.com>
2018-02-08 00:05:21 +0200

    Test Commit

--- Haverim/Startup.cs
+++ Haverim/Startup.cs
@@ -33,6 +33,7 @@ namespace Haverim
             connection = @"Server=(localdb)\mssqllocaldb;Database=HaverimProjectMock;Trusted_Connection=True;ConnectRetryCount=0";
             services.AddDbContext<HaverimContext>(options => options.UseSqlServer(connection));
 #endif
+            //Test
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
